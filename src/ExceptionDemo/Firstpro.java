7704f1d2c38537a212e31c5444b5828a0bd34bea
shailesh510 <shaileshcharthal@gmail.com>
2020-06-02 22:59:42 -0700

    first commit

---
+++ src/ExceptionDemo/Firstpro.java
@@ -0,0 +1,12 @@
+package ExceptionDemo;
+
+public class Firstpro {
+
+	public static void main(String[] args) {
+		int[] x={1,2,3,4};
+		try{
+		System.out.println(x[4]);
+		}catch(Atrrayindex)
+	}
+
+}
